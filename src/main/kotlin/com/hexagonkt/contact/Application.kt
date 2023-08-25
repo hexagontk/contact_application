6b3db3b26a1a4497f8d6fdc9b1d077752fbcdafb
jaguililla <juanjoaguililla@gmail.com>
2023-08-25 21:29:22 +0200

    Update Hexagon

--- src/main/kotlin/com/hexagonkt/contact/Application.kt
+++ src/main/kotlin/com/hexagonkt/contact/Application.kt
@@ -6,12 +6,17 @@ import com.hexagonkt.contact.stores.ContactStore
 import com.hexagonkt.contact.stores.UserStore
 import com.hexagonkt.contact.stores.impl.ContactStoreImpl
 import com.hexagonkt.contact.stores.impl.UserStoreImpl
+import com.hexagonkt.contact.stores.entities.User
+import com.hexagonkt.converters.ConvertersManager
 import com.hexagonkt.core.Jvm.systemSettingOrNull
+import com.hexagonkt.core.fieldsMapOfNotNull
+import com.hexagonkt.core.requireString
 import com.hexagonkt.http.server.HttpServer
 import com.hexagonkt.http.server.HttpServerSettings
 import com.hexagonkt.http.server.jetty.JettyServletAdapter
 import java.net.InetAddress
 import java.net.URL
+import java.time.LocalDateTime
 
 lateinit var server: HttpServer
 
@@ -34,8 +39,51 @@ fun createJwtService(): JwtServiceImpl {
     return JwtServiceImpl(URL(jwtKeyStore), jwtKeyPassword, jwtKeyAlias)
 }
 
-fun createUserStore(): UserStore =
-    UserStoreImpl()
+fun createUserStore(): UserStore {
 
-fun createContactStore(): ContactStore =
-    ContactStoreImpl()
+    ConvertersManager.register(User::class to Map::class) {
+        fieldsMapOfNotNull(
+            User::id to it.id,
+            User::email to it.email,
+            User::username to it.username,
+            User::password to it.password,
+        )
+    }
+
+    ConvertersManager.register(Map::class to User::class) {
+        User(
+            id = it.requireString(User::id),
+            email = it.requireString(User::email),
+            username = it.requireString(User::username),
+            password = it.requireString(User::password),
+        )
+    }
+
+    return UserStoreImpl()
+}
+
+fun createContactStore(): ContactStore {
+
+//    ConvertersManager.register(Contact::class to Map::class) {
+//        fieldsMapOfNotNull(
+//            Contact::id to it.id,
+//            Contact::userId to it.userId,
+//
+//            Contact::text to it.text,
+//            Contact::date to it.date.toString(),
+//            Contact::dateString to it.dateString,
+//        )
+//    }
+//
+//    ConvertersManager.register(Map::class to Contact::class) {
+//        Contact(
+//            id = it.requireString(Message::id),
+//            userId = it.requireString(Message::userId),
+//            text = it.requireString(Message::text),
+//            date = it.requireString(Message::date).let(LocalDateTime::parse),
+//            dateString = it.requireString(Message::dateString),
+//        )
+//    }
+
+    return ContactStoreImpl()
+}
